f283495fea97dea72614d96d391a3297d5bfbd29
Judy Mwai <juddiemukami@gmail.com>
2022-09-08 16:52:52 +0300

    adds delete and patch method to the application controller file

--- blog-server/app/controllers/application_controller.rb
+++ blog-server/app/controllers/application_controller.rb
@@ -16,4 +16,40 @@ class ApplicationController < Sinatra::Base
         post.to_json
     end
 
-   
+    post '/reviews' do
+        post = Review.create(
+            comment: params[:comment],
+            post_id: params[:post_id]
+        )
+        post.to_json
+    end
+
+    get '/reviews' do
+        post =Review.all
+        post.to_json
+    end
+
+    patch '/posts/:id' do
+        post = Post.find(params[:id])
+        post.update(
+            Content: params[:Content],
+            Author: params[:Author],
+            Title: params[:Title]
+        )
+        post.to_json
+
+    end
+
+    delete '/posts/:id' do
+        post = Post.find(params[:id])
+        post.delete
+        post.to_json
+    end
+
+    delete '/reviews/:id' do
+        post = Review.find(params[:id])
+        post.delete
+        post.to_json
+    end
+
+end
