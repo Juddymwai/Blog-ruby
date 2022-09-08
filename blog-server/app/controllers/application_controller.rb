8b472acde9232971de8898b801e28077889364f3
Judy Mwai <juddiemukami@gmail.com>
2022-09-08 16:50:58 +0300

    adds post command to enable posting in the application controller file

--- blog-server/app/controllers/application_controller.rb
+++ blog-server/app/controllers/application_controller.rb
@@ -4,3 +4,16 @@ class ApplicationController < Sinatra::Base
         post = Post.all
         post.to_json(include: :reviews)
     end
+
+    post '/posts' do
+
+        post= Post.create(
+
+            Title: params[:Title],
+            Author: params[:Author],
+            Content: params[:Content]
+        )
+        post.to_json
+    end
+
+   
